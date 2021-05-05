67c818ecfbf3dbd3d0d6205548e34ef47419123a
endiesworld <endiesworld@gmail.com>
2021-05-05 17:00:14 +0100

    feat: Added authentication and search menue

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,30 @@
+import CreateLink from './CreateLink';
+import Header from './Header';
+import Login from './Login';
 import LinkList from './LinkList';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Search from './Search';
 
 function App() {
   return (
-    <div>
-      <LinkList />
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+           <Route
+          exact
+          path="/"
+          render={() => <Redirect to="/new/1" />}
+        />
+          <Route
+            exact
+            path="/create"
+            component={CreateLink}
+          />
+           <Route exact path="/login" component={Login} />
+           <Route exact path="/search" component={Search} />
+        </Switch>
+      </div>
     </div>
   );
 }
